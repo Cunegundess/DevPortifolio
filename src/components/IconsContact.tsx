555214144e8dce4412d1c0a41de502acfe002bda
lucas.cunegundes <lucas.cunegundes@youtan.com.br>
2024-01-23 17:45:14 -0300

    [ADD] ContactLinks

--- src/components/IconsContact.tsx
+++ src/components/IconsContact.tsx
@@ -4,7 +4,9 @@ import { BiLogoGithub, BiLogoGmail, BiLogoLinkedin } from "react-icons/bi";
 function IconsContact() {
   return (
     <div className="flex gap-5 justify-between">
-      <motion.button
+      <motion.a
+        href="https://www.linkedin.com/in/lucas-cunegundes/"
+        target="_blank"
         className="outline outline-1 outline-zinc-400/50 shadow-lg p-1 rounded-full border-zinc-400"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
@@ -13,8 +15,10 @@ function IconsContact() {
           className="text-zinc-950 dark:text-zinc-50 hover:text-blue-800 dark:hover:text-blue-800"
           size={38}
         />
-      </motion.button>
-      <motion.button
+      </motion.a>
+      <motion.a
+        href="https://www.github.com/Cunegundess/"
+        target="_blank"
         className="outline outline-1 outline-zinc-400/50 shadow-lg p-1 rounded-full border-zinc-400"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
@@ -23,8 +27,10 @@ function IconsContact() {
           className="text-zinc-950 dark:text-zinc-50 hover:text-zinc-500 dark:hover:text-zinc-400"
           size={38}
         />
-      </motion.button>
-      <motion.button
+      </motion.a>
+      <motion.a
+        href="mailto:lucascsantana6@gmail.com"
+        target="_blank"
         className="outline outline-1 outline-zinc-400/50 shadow-lg p-1 rounded-full border-zinc-400"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
@@ -33,7 +39,7 @@ function IconsContact() {
           className="text-zinc-950 dark:text-zinc-50 hover:text-red-800 dark:hover:text-red-800"
           size={38}
         />
-      </motion.button>
+      </motion.a>
     </div>
   );
 }
