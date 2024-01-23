5aba0f7449a3756b1804a53fdfdc5baecc1a8920
lucas.cunegundes <lucas.cunegundes@youtan.com.br>
2024-01-23 15:43:07 -0300

    [FIX h-screen

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,6 @@
 function ProjectsPage() {
   return (
-    <div id="Projects" className="min-h-screen flex flex-col lg:flex-row">
+    <div id="Projects" className="h-screen flex flex-col lg:flex-row">
       <div className="lg:w-2/5 flex-col lg:ml-8 mt-28 lg:mt-[200px] justify-end lg:items-center lg:pr-4 px-5 text-center">
         <h1 className="text-zinc-950 dark:text-zinc-50 text-7xl font-bold pb-5 lg:pb-5">
           Projects
