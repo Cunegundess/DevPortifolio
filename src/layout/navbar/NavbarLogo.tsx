1d056cda703b810141c3b6b5065686bfe3cce049
Cunegundes <lucascsantana6@gmail.com>
2024-01-24 16:15:13 -0300

    [ADD] avatar shadcnui

--- src/layout/navbar/NavbarLogo.tsx
+++ src/layout/navbar/NavbarLogo.tsx
@@ -2,6 +2,10 @@ import { MdTerminal } from "react-icons/md";
 
 function NavbarLogo() {
   return <MdTerminal size={30} />;
+        // <Avatar>
+        //   <AvatarFallback>LC</AvatarFallback>
+        //   <AvatarImage src="https://github.com/cunegundess.png" />
+        // </Avatar>
 }
 
 export default NavbarLogo;
