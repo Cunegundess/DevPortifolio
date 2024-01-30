91f893ecd8dbc7ff83ef9744b12a16a4781f903c
lucas.cunegundes <lucas.cunegundes@youtan.com.br>
2024-01-30 17:00:38 -0300

    [FEAT Send emails

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,6 @@
+import emailjs from "@emailjs/browser";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { z } from "zod";
@@ -9,10 +10,10 @@ import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 
 const contactFormSchema = z.object({
-  name: z
+  user_name: z
     .string()
     .min(1, "Oops! Looks like you forgot to tell me your name 😅"),
-  email: z
+  user_email: z
     .string()
     .email(`Hmm... This email looks weird 🤔 Are you sure that's your email?`),
   message: z.string().min(1, "Your message is empty... 📝"),
@@ -28,8 +29,17 @@ export default function ContactForm() {
     resolver: zodResolver(contactFormSchema),
   });
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const ContactForm = useRef<HTMLFormElement | null>(null);
 
-  function handleFormFields(data: contactFormFieldsType) {
+  function HandleFormFields(data: contactFormFieldsType) {
+    if (ContactForm.current) {
+      emailjs.sendForm(
+        "service_9u4514f",
+        "template_8niuaq3",
+        ContactForm.current,
+        "5BVPUFtFYvjtZz-IB"
+      );
+    }
     setFormSubmitted(!formSubmitted);
     toast("🥳 Your message was sent!! ", {
       description: "Thank you, I'll respond right away",
@@ -46,34 +56,38 @@ export default function ContactForm() {
     <Card className="rounded-lg shadow-lg bg-gray-50 dark:bg-gray-900">
       <CardContent className="p-0">
         <form
-          onSubmit={handleSubmit(handleFormFields)}
+          ref={ContactForm}
+          onSubmit={handleSubmit(HandleFormFields)}
           className="flex flex-col mx-auto p-4 gap-2"
         >
           <Input
             type="text"
             placeholder="Enter your name"
+            id="name"
             className="w-full p-2  bg-gray-200 dark:bg-gray-700 rounded"
-            {...register("name", { required: true })}
+            {...register("user_name", { required: true })}
           />
-          {errors.name && (
+          {errors.user_name && (
             <p className="text-sm font-bold text-red-600">
-              {errors.name.message}
+              {errors.user_name.message}
             </p>
           )}
           <Input
             type="email"
             placeholder="Enter your email"
+            id="email"
             className="w-full p-2  bg-gray-200 dark:bg-gray-700 rounded"
-            {...register("email", { required: true })}
+            {...register("user_email", { required: true })}
           />
-          {errors.email && (
+          {errors.user_email && (
             <p className="text-sm  text-red-600 font-extrabold">
-              {errors.email.message}
+              {errors.user_email.message}
             </p>
           )}
           <Textarea
             placeholder="Type your message here"
             className="w-full h-20 p-2  bg-gray-200 dark:bg-gray-700 rounded"
+            id="message"
             {...register("message", { required: true })}
           />
           {errors.message && (
